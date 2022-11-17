4e93208dd1a15a7e976be969fe992f47845296f0
Aryan <adusharma22@gmail.com>
2022-11-18 02:39:17 +0530

    stego v2.0

--- decode.py
+++ decode.py
@@ -7,7 +7,7 @@ alphabet = string.ascii_uppercase
 
 word = ''
 
-img = Image.open(r'C:\Users\adush\Downloads\encoded.png')
+img = Image.open(r'C:\Users\adush\Downloads\encoded.png')   # enter path to image here
 pixelsNew = img.load()
 number_of_chars = pixelMap[777, 777]
 for height in range(0, 1920, 10):
